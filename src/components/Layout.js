24dd652538a7b0e1c3ea82b847a4a74501e829c2
Aseem Chopra <aseemchopra@protonmail.com>
2021-08-25 03:44:45 +0530

    ✨ Coded Logic

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,13 +1,59 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './layout.css'
+import logo from '../assets/img/logo.png'
+import Output from './Output';
 
 const Layout = (props) => {
-    const handleClick =() =>{}
+    let [input, setInput] = useState('0')
+    let [result, setResult] = useState('')
+    const handleClick =(event) =>{
+        const value = event.target.value
+        if (value === '='){
+            if (input !== ''){
+                let res = ''
+                try{
+                    // eslint-disable-next-line
+                    res = eval(input)
+                }catch(err){
+                    setResult('Math error')
+                }
+                if (res === undefined){
+                    setResult('Math error')
+                }
+                else {
+                    setResult(input + '=')
+                    setInput(res)
+                }
+            }
+        }
+        else if (value === 'C'){
+            setInput('0')
+            setResult('')
+        }
+        else if (value === 'DEL'){
+            let str = input
+            if(str.length === 1){
+                setInput('0')
+            }
+            else{
+            str = str.substr(0,str.length-1)
+            setInput(str)
+            }
+        }
+        else if (input === '0'){
+            setInput(value)
+        }
+        else {
+            setInput((input+=value))
+        }
+    }
     return(
         <div className="frame">
             <div className="calculator">
                 <br></br>
-                {/* <img /> */}
+                <Output user ={input} answer = {result} />
+                <img alt="logo.png" src={logo} height='70px' width='200px' style={{display:'block', marginLeft: 'auto', marginRight: 'auto',paddingTop: '1px' }} />
+
                 <div className="keys">
                     <input type="button" value={'C'} className='button clear' onClick={handleClick}></input>
                     <input type="button" value={'DEL'} className='button clear' onClick={handleClick}></input>
@@ -35,6 +81,8 @@ const Layout = (props) => {
 
 
                 </div>
+
+
             </div>
            
         </div>
